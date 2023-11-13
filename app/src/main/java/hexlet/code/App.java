68981379e2fc55395358559fe70deb36088851bd
Ansar Zhumabekov <ans1k98@mail.ru>
2023-11-13 17:45:34 +0600

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greet();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        String userAnswer = scanner.next();
+        System.out.println("Your choice: " + userAnswer);
+        System.out.println();
+
+        if (userAnswer.equals("1")) {
+            Cli.greet();
+        }
+        scanner.close();
     }
 }

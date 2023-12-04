a0b3d188c35032b4a406c9adc964de3823ddd089
Ansar Zhumabekov <ans1k98@mail.ru>
2023-12-04 14:42:38 +0600

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,7 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         String userAnswer = scanner.next();
         System.out.println("Your choice: " + userAnswer);
@@ -17,6 +18,8 @@ public class App {
             Cli.greet();
         } else if (userAnswer.equals("2")) {
             Even.gameEven();
+        } else if (userAnswer.equals("3")) {
+            Calc.gameCalc();
         }
         scanner.close();
     }

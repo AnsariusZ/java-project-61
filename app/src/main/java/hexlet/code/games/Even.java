22ea73700c9c796f52c139ec39f7e23e1f7718db
Ansar Zhumabekov <ans1k98@mail.ru>
2023-12-10 21:51:18 +0600

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,33 +1,28 @@
 package hexlet.code.games;
 
-import java.util.Scanner;
+import hexlet.code.Engine;
 import java.util.Random;
 
 public class Even {
-    public static void gameEven() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'");
-        Scanner scanner1 = new Scanner(System.in);
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    private static String[] generateNumbers() {
         Random random = new Random();
-        var i = 0;
-        while(i < 3) {
-            int number = random.nextInt(100);
-            System.out.println("Question: " + number);
-            String answer = scanner1.nextLine();
-            System.out.println("Your answer: " + answer);
-            if ((number % 2 == 0 && answer.equals("yes")) || (number % 2 != 0 && answer.equals("no"))) {
-                System.out.println("Correct!");
-                i++;
-            } else {
-                System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was '" + (number % 2 == 0 ? "yes" : "no") + "'.");
-                System.out.println("Let's try again");
-                scanner1.close();
-            }
+        int number = random.nextInt(100);
+        String question = String.valueOf(number);
+        String answer = isEven(number) ? "yes" : "no";
+        return new String[]{question, answer};
+    }
+
+    public static void gameEven() {
+        final var words = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+        String[][] numbers = new String[3][2];
+        for (int i = 0; i < 3; i += 1) {
+            numbers[i] = generateNumbers();
         }
-        System.out.println("Congratulations, " +userName + "!");
+
+        Engine.run(words, numbers);
     }
 }

94dc859724b0cef899c50ef74c87ccac3d714e3c
Ansar Zhumabekov <ans1k98@mail.ru>
2023-12-10 21:51:01 +0600

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,40 +1,32 @@
 package hexlet.code.games;
 
 import java.util.Random;
-import java.util.Scanner;
+import hexlet.code.Engine;
 
 public class Calc {
-    public static void gameCalc() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
-        System.out.println("What is the result of the expression?");
-        Scanner scanner1 = new Scanner(System.in);
+    private static String getRandomOperator() {
         Random random = new Random();
-        var i = 0;
-        while (i < 3) {
+        int randomNumber = random.nextInt(3);
+        String[] operators = {"+", "-", "*"};
+        return operators[randomNumber];
+    }
+    private static String[] generateNumbers() {
+        Random random = new Random();
         int number1 = random.nextInt(100);
         int number2 = random.nextInt(100);
-        String[] operators = {"+", "-", "*"};
-        int number3 = random.nextInt(3);
-        var randomOperator = operators[number3];
-        String randomExample = number1 + "" + randomOperator + "" + number2;
-        System.out.println("Question: " + randomExample);
-        String answer = scanner1.nextLine();
-        String checkAnswer = String.valueOf(calculate(randomOperator, number1, number2));
-        System.out.println("Your answer: " + answer);
-        if (answer.equals(checkAnswer)) {
-            System.out.println("Correct!");
-            i++;
-        } else {
-            System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was " + "'" + checkAnswer + "'");
-            System.out.println("Let's try again, " + userName);
-            scanner1.close();
+        String operator = getRandomOperator();
+        String question = number1 + "" + operator + "" + number2;
+        String answer = String.valueOf(calculate(operator, number1, number2));
+        return new String[]{question, answer};
+    }
+    public static void gameCalc() {
+        final var words = "What is the result of the expression?";
+        String[][] numbers = new String[3][2];
+        for (int i = 0; i < 3; i += 1) {
+            numbers[i] = generateNumbers();
         }
-        }
-        System.out.println("Congratulations, " + userName);
+
+        Engine.run(words, numbers);
     }
     private static int calculate(String operator, int number1, int number2) {
         switch (operator) {

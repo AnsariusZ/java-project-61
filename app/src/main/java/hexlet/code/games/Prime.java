5b4d31d011ff5f9c00ad455d2eb699baef02cdbd
Ansar Zhumabekov <ans1k98@mail.ru>
2023-12-11 20:37:21 +0600

    update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -4,10 +4,10 @@ import java.util.Random;
 import hexlet.code.Engine;
 public class Prime {
     public static boolean isPrime(int number) {
-        if (number <= 1) {
+        if (number < 2) {
             return false;
         }
-        for (int i = 2; i < Math.sqrt(number); i++) {
+        for (int i = 2; i < Math.sqrt(number); i += 1) {
             if (number % i == 0) {
                 return false;
             }

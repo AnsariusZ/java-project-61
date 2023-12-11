2fc6ebe52b5d8d5b75f204cb83b8d39663a19c36
Ansar Zhumabekov <ans1k98@mail.ru>
2023-12-11 20:26:17 +0600

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,6 @@
 package hexlet.code;
 
-import hexlet.code.games.Calc;
-import hexlet.code.games.Even;
-import hexlet.code.games.GCD;
-import hexlet.code.games.Progression;
+import hexlet.code.games.*;
 
 import java.util.Scanner;
 
@@ -16,6 +13,7 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         String userAnswer = scanner.next();
         System.out.println("Your choice: " + userAnswer);
@@ -31,6 +29,8 @@ public class App {
             GCD.gameGcd();
         } else if (userAnswer.equals("5")) {
             Progression.gameProgression();
+        } else if (userAnswer.equals("6")) {
+            Prime.gamePrime();
         }
         scanner.close();
     }

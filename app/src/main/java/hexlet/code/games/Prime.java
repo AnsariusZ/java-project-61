4cb82904ae46bea70b395e7ffa4db071c5e47e6c
Ansar Zhumabekov <ans1k98@mail.ru>
2023-12-11 20:44:11 +0600

    update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -7,7 +7,7 @@ public class Prime {
         if (number < 2) {
             return false;
         }
-        for (int i = 2; i < Math.sqrt(number); i += 1) {
+        for (int i = 2; i <= Math.sqrt(number); i += 1) {
             if (number % i == 0) {
                 return false;
             }
